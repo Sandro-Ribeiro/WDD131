d50c2181ef73d672257ccd3097b401ec8fe5d564
Sandro Ribeiro <santunesribeiro@outlook.com>
2025-11-11 15:21:34 -0300

    W02 Assignment: Picture Album - js

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,12 +3,21 @@ document.getElementById("currentYear").innerHTML = `&copy;${d.getFullYear()}`;
 document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
 
 const hambutton = document.querySelector('#hambutton');
+const navigation = document.querySelector('#navmenu');
+const headerTitle = document.querySelector('h1');
 
-hambutton.addEventListener('click', () => {
-  document.querySelector('h1').classList.toggle('show');
-  document.querySelector('#navmenu').classList.toggle('show');
-  hambutton.classList.toggle('show');
-});
+if (hambutton) {
+  hambutton.addEventListener('click',
+    () =>
+    {
+      navigation.classList.toggle('show');
+      hambutton.classList.toggle('show');
+      headerTitle.classList.toggle('show');
+      const isExpanded = navigation.classList.contains('show');
+      hambutton.setAttribute('aria-expanded', isExpanded);
+    }
+  );
+}
 
 function toggleActive(element) {
   element.classList.toggle("active");
