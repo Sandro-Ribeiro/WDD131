c6fd623d5ae84571bd70517902e45edc5d93500d
Sandro Ribeiro <santunesribeiro@outlook.com>
2025-12-10 23:40:38 -0300

    W06 Project middle

--- project/scripts/coisasdocerrado.js
+++ project/scripts/coisasdocerrado.js
@@ -1,25 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {
+function calculateWindChill(tempC, speed) {
+    return 13.12 + (0.6215 * tempC) - (11.37 * Math.pow(speed, 0.16)) + (0.3965 * tempC * Math.pow(speed, 0.16));
+}
+
+
+function updateFooterDates() {
+
+    const currentYearElement = document.getElementById("currentYear");
     const currentYear = new Date().getFullYear();
-    const yearElement = document.getElementById("currentYear");
-    if (yearElement) {
-        yearElement.textContent = currentYear;
+    
+    if (currentYearElement) {
+        currentYearElement.textContent = `©${currentYear}`;
     }
 
-    const lastModified = document.lastModified;
-    const modifiedElement = document.getElementById("lastModified");
+    const lastModifiedElement = document.getElementById("lastModified");
+    const lastModifiedDate = document.lastModified;
 
-    if (modifiedElement) {
-        modifiedElement.textContent = `Last Modified: ${lastModified}`;
+    if (lastModifiedElement) {
+        lastModifiedElement.textContent = `Last Modification: ${lastModifiedDate}`;
     }
+}
 
-    function calculateWindChill(tempC, speed) {
-        return 13.12 + (0.6215 * tempC) - (11.37 * Math.pow(speed, 0.16)) + (0.3965 * tempC * Math.pow(speed, 0.16));
-    }
 
-    let tempC = 23;
+document.addEventListener("DOMContentLoaded", () => {
+
+    updateFooterDates(); 
+
+    let tempC = 23; 
     let speed = 8.5;
+
+    [cite_start]
     if (tempC <= 10 && speed > 4.8) {
-    document.getElementById("wcf").innerHTML = `${calculateWindChill(tempC, speed).toFixed(1)} °C`;
+        const wcf = calculateWindChill(tempC, speed);
+        document.getElementById("wcf").innerHTML = `${wcf.toFixed(1)} °C`;
     } else {
         document.getElementById("wcf").innerHTML = "N/A";
     }
